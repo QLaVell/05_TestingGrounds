abb9e09fdb3b44435d93761ce8174ee0381bd199
Quintin Oliver <quint.oliver@gmail.com>
2018-08-17 08:48:39 -0400

    Changed from inheritance to composition 20. Lecture 243: Composition over Inheritance

--- Source/TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -1,21 +1,28 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ChooseNextWaypoint.h"
-#include "BehaviorTree/BlackboardComponent.h"
 #include "AIController.h"
-#include "PatrollingGuard.h" // TODO Remove coupling
+#include "PatrolRoute.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) {
+	// Get patrol route
+	APawn* ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	UPatrolRoute* PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRoute>();
+	if (!ensure(PatrolRoute)) { return EBTNodeResult::Failed; }
+
 	// Get the patrol points
-	APatrollingGuard* PatrollingGuard = Cast<APatrollingGuard>(OwnerComp.GetAIOwner()->GetPawn());
-	TArray<AActor*> PatrolPoints = PatrollingGuard->PatrolPointsCPP;
+	// warn about empty patrol routes
+	TArray<AActor*> PatrolPoints = PatrolRoute->GetPatrolPoints();
+	if(PatrolPoints.Num() == 0) { 
+		UE_LOG(LogTemp, Warning, TEXT("A guard is missing patrol points."))
+		return EBTNodeResult::Failed;
+	}
 
 	// Set the next waypoint
 	UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
 	int32 Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
 	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
-
-	// TODO Protect against empty patrol routes
 
 	// Cycle the index
 	int32 NextIndex = (Index + 1) % PatrolPoints.Num();
